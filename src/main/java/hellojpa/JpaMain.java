cb42dcc45abb4e086078b8424311174335571468
eomjunseong <hi_man159@naver.com>
2022-06-09 11:09:53 +0900

    type...

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,21 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
-            member.changeTeam(team);
+            member.setTeam(team);
+            member.setType(MemberType.ADMIN);
             em.persist(member);
 
-            String query ="select m from Member m right join m.team t";
-            List<Member> resultList = em.createQuery(query, Member.class).getResultList();
-            System.out.println(resultList.get(0));
-
+//            String query ="select m.username,'HELLO', TRUE,m.type from Member m where m.type= hellojpa.MemberType.ADMIN";
+            String query ="select m.username,'HELLO', TRUE,m.type from Member m where m.type= :type";
+            List<Object[]> list = em.createQuery(query)//            String query ="select m.username,'HELLO', TRUE from Member m ";
+                    .setParameter("type",MemberType.ADMIN)
+                    .getResultList();
+            for (Object[] objects : list) {
+                System.out.println(objects[0]);
+                System.out.println(objects[1]);
+                System.out.println(objects[2]);
+                System.out.println(objects[3]);
+            }
 
             tx.commit();//member는 준영속 상태가 되어 name이 바뀐 것이 적용되지 않는다.
         }catch(Exception error){
