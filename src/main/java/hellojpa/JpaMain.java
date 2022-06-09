80d13737c6d603eca51b652e13ff1d8c1b4406ff
eomjunseong <hi_man159@naver.com>
2022-06-09 10:23:04 +0900

    select

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,11 +17,16 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member singleResult = em.createQuery(
-                            "select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println(member.getAge());
+            //1. Query 타입으로 조회
+            //2. Object[] 타입으로 조회
+            //3. NEW 명령어로 조회 , 패키지명 써줘야함 
+
+            List<MemberDTO> resultList = em.createQuery(
+                            "select new hellojpa.MemberDTO(m.username, m.age) from Member m ", MemberDTO.class)
+                    .getResultList();
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println(memberDTO.getAge());
+            System.out.println(memberDTO.getUsername());
 
 
             tx.commit();//member는 준영속 상태가 되어 name이 바뀐 것이 적용되지 않는다.
