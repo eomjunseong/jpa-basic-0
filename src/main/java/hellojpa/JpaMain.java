6ad10e1076e8064f5144dbb4a33f6018bcfa2020
eomjunseong <hi_man159@naver.com>
2022-06-10 09:43:35 +0900

    join fetch

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,32 +12,39 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("팀A");
+            Team team2 = new Team();
+            team2.setName("팀B");
+            em.persist(team1);
+            em.persist(team2);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
-/* case
-            String query =
-                    "select " +
-                            "case when m.age<=10 then '학생요금' "+
-                            "     when m.age>=60 then '성인요금' " +
-                            " else '일반요금' end " +
-                            "from Member m";
-            List<String> result = em.createQuery(query, String.class).getResultList();
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }*/
-//coalesce
-            String query = "select coalesce(m.username,'이름없음') from Member m ";
-            List<String> result = em.createQuery(query, String.class).getResultList();
-            for (String s : result) {
-                System.out.println("s = " + s);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(team1);
+            member1.setAge(10);
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team1);
+            member2.setAge(10);
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setTeam(team2);
+            member3.setAge(10);
+
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+//            String query="select m from Member m join fetch m.team";
+            String query="select distinct t from Team t join fetch t.members";
+            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+            for (Team s : resultList) {
+                System.out.println("s = " + s.getName() + " | "+s.getMembers().size());
             }
 
             tx.commit();//member는 준영속 상태가 되어 name이 바뀐 것이 적용되지 않는다.
